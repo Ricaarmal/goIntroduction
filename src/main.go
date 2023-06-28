5c210c9385ebe2e35fa42429e30b73b1e98695a2
Ricardo Manzano <noteplay101@hotmail.com>
2023-06-28 02:30:40 -0600

    Listo para el push con SSH

--- src/main.go
+++ src/main.go
@@ -3,5 +3,47 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Mundo")
+	//Declaracion de constantes
+	const pi float64 = 3.1416
+	const pi2 = 3.14
+	fmt.Println("pi:", pi)
+	fmt.Println("p2:", pi2)
+
+	//Declaracion de variables enteras solo 3 formas.
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println("base", base)
+	fmt.Println("altura", altura)
+	fmt.Println("area", area)
+
+	//Zero Values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+	const baseCuadrado int = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuadrado:", areaCuadrado)
+
+	//Suma
+	var x int = 12
+	y := 14
+
+	resultado := x + y
+	fmt.Println("Resultado:", resultado)
+
+	//Multiplicacion
+	resultado = x * y
+	fmt.Println("Multiplicacion", resultado)
+
+	//division
+	resultado = x / y
+	fmt.Println("Division", resultado)
+
 }
